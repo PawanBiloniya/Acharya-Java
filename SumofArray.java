36c49e2d4fb478fca7be4da401037ba0423ca842
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 23:11:03 +0530

    Implement SumOfArray class to calculate sum

---
+++ SumofArray.java
@@ -0,0 +1,17 @@
+package swap;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		
+	int [] arr= {21,10,16,38};
+	int sum=0;
+	
+	for(int i = 0; i< arr.length; i++) {
+		sum=sum +arr[i];
+	}
+	System.out.println("Sum Of the Array = " + sum);
+
+	}
+
+}
