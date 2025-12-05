2cbd6136f16e8d14e94ff5b5bf5acf7c2ac111b5
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 23:03:21 +0530

    Fix sum calculation in SumOf.java

---
+++ SumOf.java
@@ -0,0 +1,19 @@
+package swap;
+
+import java.util.Scanner;
+
+public class SumOf {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while(num > 0) {
+            sum = sum + (num % 10);
+            num = num / 10;
+        }
+
+        System.out.println("Sum of The Digits: " + sum);
+    }
+}
