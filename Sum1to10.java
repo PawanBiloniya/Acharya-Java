97b85312426f26cacf49d77e5cf043bdde47c9a2
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 22:52:57 +0530

    Create Sum1to10.java

---
+++ Sum1to10.java
@@ -0,0 +1,14 @@
+package swap;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+	int sum=0;
+	
+	for(int i=1;i<10;i++) {
+		sum=sum+1;
+	}
+	System.out.println("sum="+sum);
+	}
+
+}
