d1ab608579a9270268bc76ac38ef34df07196450
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 22:51:06 +0530

    Add leap year checker in Leap.java

---
+++ Leap.java
@@ -0,0 +1,18 @@
+package leap;
+import java.util.Scanner;
+
+public class leap {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a year:");
+		int year=sc.nextInt();
+		if((year%4)==0) {
+			System.out.println(year +"is a leap year");
+		}else {
+			System.out.println(year+"is not a leap year");
+		}
+		sc.close();
+	}
+
+}
