423ab6822b09d6b3cdf2f7cc96f1d61dd9fa5371
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 22:51:39 +0530

    Add Swap.java to swap two integer values

---
+++ Swap.java
@@ -0,0 +1,18 @@
+package swap;
+
+public class swap {
+
+	public static void main(String[] args) {
+	int a=15;
+	int b=30;
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println("a is:"+a);
+	System.out.println("b is :"+b);                
+	
+
+	}
+
+}
