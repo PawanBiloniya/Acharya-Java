689b04373045b0fb0c7e37c4adf0b5daf9db4eee
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 22:50:35 +0530

    Add Array class with basic input and output

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Array {
+
+	public static void main(String[] args) {
+	int[]a= {11,25,39,48,58,68};
+	
+	System.out.println(a[3]);
+	System.out.println("Enter the array elements");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a number is:"+ sc.nextInt());
+
+
+	}
+
+}
