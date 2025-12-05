65d3b768a08272cf31887c53847806f33c7afd79
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 22:55:40 +0530

    Implement TOperator to find maximum of five integers

---
+++ TOperator.java
@@ -0,0 +1,15 @@
+package swap;
+
+public class TOperator {
+
+	public static void main(String[] args) {
+		int a = 500;
+		int b = 100;
+		int c =300;
+		int d =450;
+		int e = 690;
+		int max = (a > b) ? ( (a>c) ? ( (a>d) ? ( (a>e) ? a : e ) : ( (d>e) ? d : e ) ) : ( (c>d) ? ( (c>e) ? c : e ) : ( (d>e) ? d : e ) ) ) : ( (b>c) ? ( (b>d) ? ( (b>e) ? b : e ) : ( (d>e) ? d : e ) ) : ( (c>d) ? ( (c>e) ? c : e ) : ( (d>e) ? d : e ) ) );
+		System.out.println("Max value: " + max);                                     
+	}
+}	
+	
