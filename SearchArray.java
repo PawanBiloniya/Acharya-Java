bedc45a4b281976cd50e33c51a203d2a72ebbeb7
Pawan Kumar Biloniya <pawan.2246348@mygyanvihar.com>
2025-12-05 23:13:07 +0530

    Implement search functionality in SearchArray class
    Fix logic errors in search algorithm and improve output.

---
+++ SearchArray.java
@@ -0,0 +1,26 @@
+package swap;
+
+
+public class SearchArray {
+
+	public static void main(String[] args) {
+		int [] arr= {14,25,36,12,33,12,55};
+		int target= 5;
+		boolean flag= false;
+		for(int i=0; i<arr.length; i++) {
+			if (arr[i]== target);
+			flag= true;
+			break;
+					
+		} if (flag== true);{
+			
+		if (flag == true) System.out.println("Target Existe in Array");
+		else System.out.println("Target Missing in Arrayy");
+		}
+	}
+}
+
+			
+		// TODO Auto-generated method stub
+
+	
